f59676640604d083676f91d072b92dce58f02df9
DocteurDario <docteurdario@gmail.com>
2022-10-29 19:38:05 -0300

    Se agrega el metodo listar datosUsuarios

--- DatoCallCenter/AccesoDatos.cs
+++ DatoCallCenter/AccesoDatos.cs
@@ -3,10 +3,54 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
 
 namespace DatoCallCenter
 {
     public class AccesoDatos
     {
+        private SqlConnection conexion;
+        private SqlCommand comando;
+        private SqlDataReader lector;
+        public SqlDataReader Lector
+        {
+            get { return lector; }
+        }
+
+        public AccesoDatos()
+        {
+            conexion=new SqlConnection("server=.\\SQLEXPRESS; database=CallCenter; integrated security=true");
+            comando=new SqlCommand();
+        }
+        public void setearQuery(string query)
+        {
+            comando.CommandType=System.Data.CommandType.Text;
+            comando.CommandText=query;
+        }
+        public void ejecutarLectura()
+        {
+            comando.Connection = conexion;
+            try
+            {
+                conexion.Open();
+                lector = comando.ExecuteReader();
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+        public void cerrarConexion()
+        {
+            if (lector != null) 
+            {
+                lector.Close(); 
+            }
+            conexion.Close();
+        }
+
     }
+
+
 }
--- DatoCallCenter/DatosUsuarios.cs
+++ DatoCallCenter/DatosUsuarios.cs
@@ -3,10 +3,44 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
+using clases;
 
 namespace DatoCallCenter
 {
     public class DatosUsuarios
     {
+        public List<Usuarios> Listar() 
+        {
+            List<Usuarios> lista=new List<Usuarios>();
+            AccesoDatos datos=new AccesoDatos();
+            try
+            {
+                datos.setearQuery("Select Id, Nombre, Apellido,DNI,IdPerf From Usuarios");
+                datos.ejecutarLectura();
+                while( datos.Lector.Read())
+                {
+                    Usuarios aux = new Usuarios();
+                    aux.Id = (int)datos.Lector["Id"];
+                    aux.Nombre = (string)datos.Lector["Nombre"];
+                    aux.Apellido = (string)datos.Lector["Apellido"];
+                    aux.DNI = (string)datos.Lector["DNI"];
+                    aux.IdPerfil = (int)datos.Lector["IdPerf"];
+                    lista.Add(aux);
+                }
+                return lista;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally 
+            {
+                datos.cerrarConexion();           
+            } 
+            
+        }
+        
+
     }
 }
